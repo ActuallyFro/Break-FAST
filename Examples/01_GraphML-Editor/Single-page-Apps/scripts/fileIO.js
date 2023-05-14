e83d2b275ec272fa45d6168cddf0723ec48fab92
actuallyfro <actuallyfro@github.com>
2023-05-14 11:57:10 -0500

    {Examples | SPA GraphML Editor} Deadcode, and framing code sections for next sprints (attrs|nodes|edges into XML)

--- Examples/01_GraphML-Editor/Single-page-Apps/scripts/fileIO.js
+++ Examples/01_GraphML-Editor/Single-page-Apps/scripts/fileIO.js
@@ -1,40 +1,3 @@
-
-// // 1.3.1 Implement Export GraphML feature
-// function exportGraphMLLogic() {
-//   console.log("1.3.1.1 Implement logic for exporting graph data to GraphML format");
-// }
-
-// function exportGraphMLFeedback() {
-//   console.log("1.3.1.2 Incorporate feedback from stakeholders");
-// }
-
-// // 1.3.2 Implement Import GraphML feature
-// function importGraphMLLogic() {
-//   console.log("1.3.2.1 Implement logic for importing GraphML data for editing");
-// }
-
-// function importGraphMLFeedback() {
-//   console.log("1.3.2.2 Incorporate feedback from stakeholders");
-// }
-
-// // 1.3.3 Implement Export JSON feature
-// function exportJSONLogic() {
-//   console.log("1.3.3.1 Implement logic for exporting graph data to JSON format");
-// }
-
-// function exportJSONFeedback() {
-//   console.log("1.3.3.2 Incorporate feedback from stakeholders");
-// }
-
-// // 1.3.4 Implement Import JSON feature
-// function importJSONLogic() {
-//   console.log("1.3.4.1 Implement logic for importing JSON data for editing");
-// }
-
-// function importJSONFeedback() {
-//   console.log("1.3.4.2 Incorporate feedback from stakeholders");
-// }
-
 /*
 DEFAULT GraphML File:
 
@@ -69,7 +32,9 @@ window.defaultGraphMLHEADER = `<?xml version="1.0" encoding="UTF-8"?>
 <graphml xmlns="http://graphml.graphdrawing.org/xmlns"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
-        http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">`;
+        http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+
+    <!-- Attribute Keys -->`;
 
         // <!-- Attribute Keys -->
         // <!--     (From 'Recommended Attribute Keys' Above -^ filled in) -->
@@ -87,6 +52,9 @@ window.defaultGraphMLHEADER = `<?xml version="1.0" encoding="UTF-8"?>
         //     <!--     (Copy & paste FROM 'Entry Table') -->
   
 window.defaultGraphMLFOOTER = `
+        <!-- Node Entries -->
+
+        <!-- Edge Entries -->
     </graph>
 </graphml>`;
 
@@ -108,6 +76,6 @@ window.reprintGraphMLFile = function(objectId, graphType, data) {
     setGraphMLContent(defaultGraphMLHEADER+"\n");
     setGraphMLContentAPPEND("\n");
     // setGraphMLContentAPPEND('    <graph id="' + graphTitle + '" edgedefault="undirected|directed"></graphml>');
-    setGraphMLContentAPPEND('    <graph id="' + window.graphTitle + '" edgedefault="' + window.graphDirectionality + '"></graphml>');
+    setGraphMLContentAPPEND('    <graph id="' + window.graphTitle + '" edgedefault="' + window.graphDirectionality + '">');
     setGraphMLContentAPPEND(defaultGraphMLFOOTER);
 }
