b672d08ac8ad165f45bb95b97c61d99990f79169
actuallyfro <actuallyfro@github.com>
2023-05-14 14:22:46 -0500

    {Examples | SPA GraphML Editor} Feature creep - deadcode removal

--- Examples/01_GraphML-Editor/Single-page-Apps/scripts/fileIO.js
+++ Examples/01_GraphML-Editor/Single-page-Apps/scripts/fileIO.js
@@ -1,32 +1,3 @@
-/*
-DEFAULT GraphML File:
-
-<?xml version="1.0" encoding="UTF-8"?>
-<graphml xmlns="http://graphml.graphdrawing.org/xmlns"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
-      http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-
-      <!-- Attribute Keys -->
-      <!--     (From 'Recommended Attribute Keys' Above -^ filled in) -->
-  
-      <graph id="NAME OF GRAPH" edgedefault="undirected|directed">
-
-          <!-- Node Entries -->
-          <!--     (Copy & paste FROM 'Entry Table') -->
-          
-          <!--     Suggest: 'nameNode'; for D3.js Label -->
-          <!--     Suggest: 'descriptionNode'; for ul / li listings -->
-          <!--     Example: https://actuallyfro.github.io/SpaceEngineersMapping/ -->
-          
-          <!-- Edge Entries -->
-          <!--     (Copy & paste FROM 'Entry Table') -->
-
-      </graph>
-</graphml>
-*/
-
-
 //http://graphml.graphdrawing.org/primer/graphml-primer.html#Graph
 window.defaultGraphMLHEADER = `<?xml version="1.0" encoding="UTF-8"?>
 <graphml xmlns="http://graphml.graphdrawing.org/xmlns"
