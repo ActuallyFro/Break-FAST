ce7d7522251e1030eb927b7a570a3f6c125c5f7a
actuallyfro <actuallyfro@github.com>
2023-05-14 22:31:37 -0500

    {Examples | SPA GraphML Editor} Now with color coding

--- Examples/01_GraphML-Editor/Single-page-Apps/scripts/graphRendering.js
+++ Examples/01_GraphML-Editor/Single-page-Apps/scripts/graphRendering.js
@@ -1,3 +1,6 @@
+// Add a colors object to keep track of node colors
+let nodeColors = JSON.parse(localStorage.getItem('nodeColors')) || {};
+
 window.drawGraph = function(graphObjects, debug = false) {
     // Define the dimensions of the SVG
     const width = 1200;
@@ -41,14 +44,43 @@ window.drawGraph = function(graphObjects, debug = false) {
         .force('link', d3.forceLink(links).id(d => d.id))
         .on('tick', ticked);
 
-    // Draw nodes
-    const node = svg.selectAll('.node')
-        .data(nodes)
-        .join('circle')
-        .attr('class', 'node')
-        .attr('r', 20)
-        .attr('fill', 'lightblue')
-        .call(drag(simulation));
+    // // Draw nodes
+    // const node = svg.selectAll('.node')
+    //     .data(nodes)
+    //     .join('circle')
+    //     .attr('class', 'node')
+    //     .attr('r', 20)
+    //     .attr('fill', 'lightblue')
+    //     .call(drag(simulation));
+
+
+// Modify the node drawing code to use the color from nodeColors
+const node = svg.selectAll('.node')
+    .data(nodes)
+    .join('circle')
+    .attr('class', 'node')
+    .attr('r', 20)
+    .attr('fill', d => nodeColors[d.id] || 'lightblue') // Use the color from nodeColors if it exists
+    .call(drag(simulation))
+    .on('contextmenu', function(event, d) { // Add a right-click handler
+        event.preventDefault();
+
+        // Show the context menu at the mouse position
+        d3.select('#context-menu')
+            .style('left', `${event.pageX}px`)
+            .style('top', `${event.pageY}px`)
+            .style('display', 'block');
+
+        // Update the color picker when it changes
+        d3.select('#color-picker').node().value = nodeColors[d.id] || '#000000';
+
+        // When the color picker value changes, update nodeColors and the node color
+        d3.select('#color-picker').on('input', function() {
+            nodeColors[d.id] = this.value;
+            d3.select(event.currentTarget).style('fill', this.value);
+            localStorage.setItem('nodeColors', JSON.stringify(nodeColors));
+        });
+    });
 
     // Draw labels
     const label = svg.selectAll('.label')
@@ -63,6 +95,10 @@ window.drawGraph = function(graphObjects, debug = false) {
 
     // Update the positions of the nodes, edges, and labels
     function ticked() {
+        node.attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .style('fill', d => nodeColors[d.id] || 'lightblue');
+
         node.attr('cx', d => d.x)
             .attr('cy', d => d.y);
 
@@ -183,52 +219,95 @@ function saveNodePositions() {
 //////////////////////////////////////////////////
 // Node X/Y save and load -- LOCALSTORAGE
 function saveNodePositionsToFile() {
-    const nodePositions = {};
+    const nodeData = {};
     RenderNodes.forEach(node => {
-      nodePositions[node.id] = { x: node.x, y: node.y };
+        nodeData[node.id] = { x: node.x, y: node.y, color: nodeColors[node.id] };
     });
-    const data = JSON.stringify(nodePositions);
-    const file = new Blob([data], { type: 'application/json' }); // 'text/plain
+    const data = JSON.stringify(nodeData);
+    const file = new Blob([data], { type: 'application/json' });
     const a = document.createElement('a');
     a.href = URL.createObjectURL(file);
-    a.download = 'node-positions-configuration.json';
+    a.download = 'node-data-configuration.json';
     a.click();
-  }
+}
   
   // Add event listeners to save and load buttons
   document.addEventListener('DOMContentLoaded', () => {
-      document.getElementById('export-config-button').addEventListener('click', () => {
-      saveNodePositionsToFile();
-      // alert('Saved!');
-      });
+    //   document.getElementById('export-config-button').addEventListener('click', () => {
+    //   saveNodePositionsToFile();
+    //   // alert('Saved!');
+    //   });
   
-      document.getElementById('import-config-button').addEventListener('click', () => {
-          document.getElementById('import-config-file').click();
-      });
+    //   document.getElementById('import-config-button').addEventListener('click', () => {
+    //       document.getElementById('import-config-file').click();
+    //   });
   
-      document.getElementById('import-config-file').addEventListener('change', event => {
-          console.log('Import button clicked');
-          const file = event.target.files[0];
-          loadNodePositionsFromFile(file);
-        //   alert('Imported!');
-      });
-  });
+    //   document.getElementById('import-config-file').addEventListener('change', event => {
+    //       console.log('Import button clicked');
+    //       const file = event.target.files[0];
+    //       loadNodePositionsFromFile(file);
+    //     //   alert('Imported!');
+    //   });
+    document.getElementById('export-config-button').addEventListener('click', () => {
+        saveNodePositionsToFile();
+    });
+    
+    document.getElementById('import-config-button').addEventListener('click', () => {
+        document.getElementById('import-config-file').click();
+    });
+    
+    document.getElementById('import-config-file').addEventListener('change', event => {
+        const file = event.target.files[0];
+        loadNodePositionsFromFile(file);
+    });
+
+});
   
   // Load node positions from a text file
   function loadNodePositionsFromFile(file) {
-      console.log('Loading node positions from file...');
-      const reader = new FileReader();
-      reader.onload = event => {
-        const nodePositions = JSON.parse(event.target.result);
+    console.log('Loading node data from file...');
+    const reader = new FileReader();
+    reader.onload = event => {
+        const nodeData = JSON.parse(event.target.result);
         RenderNodes.forEach(node => {
-          const position = nodePositions[node.id];
-          if (position) {
-            node.fx = position.x;
-            node.fy = position.y;
-          }
+            const data = nodeData[node.id];
+            if (data) {
+                node.fx = data.x;
+                node.fy = data.y;
+                if (data.color) {
+                    nodeColors[node.id] = data.color; // Update nodeColors
+                }
+            }
         });
-      };
-      reader.readAsText(file);
-  }  
+        localStorage.setItem('nodeColors', JSON.stringify(nodeColors)); // Update local storage
+    };
+    reader.readAsText(file);
+} 
+
 
 //////////////////////////////////////////////////
+
+
+
+// // Modify the ticked function to update the node color
+// function ticked() {
+
+//     // rest of your code
+// }
+
+// Add an event handler to hide the context menu when you click outside of it
+window.addEventListener('click', function(event) {
+    const contextMenu = document.getElementById('context-menu');
+    if (event.target !== contextMenu && !contextMenu.contains(event.target)) {
+        contextMenu.style.display = 'none';
+    }
+});
+
+
+
+
+///////////////////
+
+
+
+//////
