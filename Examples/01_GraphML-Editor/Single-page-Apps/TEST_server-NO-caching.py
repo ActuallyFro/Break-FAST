5068142befe404396f49147b24f4630b5c7db12e
actuallyfro <actuallyfro@github.com>
2023-05-20 23:59:34 -0500

    {Examples | SPA GraphML Editor} [REFACTOR] this _SHOULD_ work... -_-

--- Examples/01_GraphML-Editor/Single-page-Apps/TEST_server-NO-caching.py
+++ Examples/01_GraphML-Editor/Single-page-Apps/TEST_server-NO-caching.py
@@ -1,11 +1,12 @@
 #!/bin/python
 import http.server
 
-class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+class No304NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
-        super().end_headers()
+        self.send_header('Last-Modified', self.date_time_string())
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    http.server.test(HandlerClass=NoCacheHTTPRequestHandler)
+    http.server.test(HandlerClass=No304NoCacheHTTPRequestHandler)
 
